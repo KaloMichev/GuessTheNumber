3fe57a24241d34fa0332cefe4063a0c37e196d49
KaloM <kaloyan.michev.highschool@svetlina.net>
2023-04-29 10:31:39 +0300

    Add project files.

---
+++ num/Program.cs
@@ -0,0 +1,30 @@
+﻿Random rnd = new Random();
+int number = rnd.Next(1, 101);
+
+while (true)
+{
+    Console.Write($"Guess a number (1-100): ");
+    string input = Console.ReadLine();
+    bool isValid = int.TryParse(input, out int playerNum);
+    if (isValid)
+    {
+        if (playerNum == number)
+        {
+            Console.WriteLine("You guessed it!");
+            break;
+        }
+        else if (playerNum > number)
+        {
+            Console.WriteLine("Too high!");
+        }
+        else if (playerNum < number)
+        {
+            Console.WriteLine("Too low!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid input.");
+    }
+    
+}
